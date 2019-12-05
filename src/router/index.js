6c1251f26b542c405faf7a054b002de80f5ac17b
DrFaelSan <rafaelvplima@gmail.com>
2019-12-05 17:06:16 -0300

    implementando página home 45%

--- src/router/index.js
+++ src/router/index.js
@@ -1,88 +1,95 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import store from '@/store/'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import store from "@/store/";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+    path: "/",
+    name: "login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue")
   },
   {
     path: "/cadastro",
     name: "cadastro",
-    component: () => import(/* webpackChunkName: "cadastro"*/ '../views/Cadastro.vue')
+    component: () =>
+      import(/* webpackChunkName: "cadastro"*/ "../views/Cadastro.vue")
   },
   {
     name: "DashBoard",
     path: "/home",
     meta: { requiresAuth: true },
-    component: () => import(/* webpackChunkName: "DashBoard"*/ "../views/DashBoard.vue")
+    component: () =>
+      import(/* webpackChunkName: "DashBoard"*/ "../views/DashBoard.vue")
   },
   {
     name: "CadastroPessoas",
     path: "/cadastropessoas",
     meta: { requiresAuth: true },
-    component: () => import(/* webpackChunkName: "CadastroPessoas"*/"../views/CadastroPessoas.vue")
+    component: () =>
+      import(
+        /* webpackChunkName: "CadastroPessoas"*/ "../views/CadastroPessoas.vue"
+      )
   },
   {
     name: "CadastroTurmas",
     path: "/cadastroturmas",
     meta: { requiresAuth: true },
-    component: () => import(/* webpackChunkName: "CadastroTurmas"*/"../views/CadastroTurmas.vue")
+    component: () =>
+      import(
+        /* webpackChunkName: "CadastroTurmas"*/ "../views/CadastroTurmas.vue"
+      )
   },
   {
     name: "Turmas",
     path: "/turmas",
     meta: { requiresAuth: true },
-    component: () => import(/* webpackChunkName: "Turmas"*/"../views/Turmas.vue")
+    component: () =>
+      import(/* webpackChunkName: "Turmas"*/ "../views/Turmas.vue")
   },
   {
     name: "Pessoas",
     path: "/pessoas",
     meta: { requiresAuth: true },
-    component: () => import(/* webpackChunkName: "Pessoas"*/"../views/AlunosEProfessores.vue")
+    component: () =>
+      import(/* webpackChunkName: "Pessoas"*/ "../views/AlunosEProfessores.vue")
   },
   {
     name: "Sobre",
     path: "/sobre",
     meta: { requiresAuth: true },
-    component: () => import(/* webpackChunkName: "Sobre"*/"../views/SobreNos.vue")
+    component: () => import(/* webpackChunkName: "Sobre"*/ "../views/Sobre.vue")
   },
-  { path: '*', redirect: '/'}
-]
+  { path: "*", redirect: "/" }
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
 router.beforeEach((to, from, next) => {
-
   /*Configura as rotas que nao precisam de autorizacao*/
-  const publicPages = ['/', '/cadastro'];
+  const publicPages = ["/", "/cadastro"];
   //Verifica se a rota que o usuario quer precisa ou não de autorizacao
   const authRequired = !publicPages.includes(to.path);
 
   //Verificar se usuario esta logado no sistema
-  const isLogado = localStorage.getItem('token')
+  const isLogado = localStorage.getItem("token");
 
-  store.state.login.loginSuccessful = isLogado == null ? false : true
+  store.state.login.loginSuccessful = isLogado == null ? false : true;
   if (store.state.login.loginSuccessful)
-    store.commit('login/loginSucesso', isLogado)
-
+    store.commit("login/loginSucesso", isLogado);
 
   //Verifica se o usuario esta logado ou nao
   if (!store.state.login.loginSuccessful)
     if (authRequired && !store.state.login.loginSuccessful)
-      return next({ path: '/' });
+      return next({ path: "/" });
 
-  next()
-  
-})
+  next();
+});
 
-
-export default router
+export default router;
---
+++ src/views/Sobre.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-app>
+    <v-content>
+      <AppBar />
+      <v-container fluid>
+        <v-row justify="center">
+          <v-col cols="12" sm="8" lg="4" md="6" xs="2">
+            <v-carousel hide-delimiters="false">
+              <v-carousel-item
+                v-for="(item, i) in items"
+                :key="i"
+                :src="item.src"
+                reverse-transition="fade-transition"
+              >
+                <v-card max-width="344" class="mx-auto">
+                  <v-list-item>
+                    <!-- <v-list-item-avatar color="blue"></v-list-item-avatar> -->
+                    <v-list-item-content>
+                      <v-list-item-title class="headline">{{item.nome}}</v-list-item-title>
+                      <v-list-item-subtitle>{{item.tipo}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
+
+                  <v-card-text>Nesse projeto ajudei a implementar o visual do das Turmas e da Página Inicial.</v-card-text>
+
+                  <v-btn icon>
+                    <v-icon>fab fa-facebook</v-icon>
+                  </v-btn>
+                  <v-card-actions>
+                    <v-btn v-for="(icon, j) in icons" :key="j" icon>
+                      <v-icon>{{icon}}</v-icon>
+                    </v-btn>
+                    <v-spacer></v-spacer>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>Facebook</v-btn>
+                    <v-btn icon>
+                      <v-icon>mdi-heart</v-icon>
+                    </v-btn>
+                    <v-btn icon>
+                      <v-icon>mdi-share-variant</v-icon>
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-carousel-item>
+            </v-carousel>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <Footer />
+  </v-app>
+</template>
+<script>
+import AppBar from "..//components/AppBar";
+import Footer from "..//components/Footer";
+export default {
+  components: {
+    AppBar,
+    Footer
+  },
+  data() {
+    return {
+      items: [
+        {
+          nome: "Rafael",
+          tipo: "Desenvolvedor Full Stack"
+        },
+        {
+          nome: "Guilherme",
+          tipo: "Desenvolvedor Full Stack"
+        },
+        {
+          nome: "André",
+          tipo: "Desenvolvedor Back-end"
+        },
+        {
+          nome: "Matheus",
+          tipo: "Desenvolvedor Black-Tropend"
+        }
+      ],
+      icons: [
+        "fab fa-facebook",
+        "fab fa-github",
+        "fab fa-twitter",
+        "fab fa-linkedin",
+        "fab fa-instagram"
+      ]
+    };
+  }
+};
+</script>
