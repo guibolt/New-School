fb1170fbc240d0d7a02d22025a93aa707739c04d
DrFaelSan <rafaelvplima@gmail.com>
2019-12-05 14:18:49 -0300

    implementando página home 40%

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -12,8 +12,14 @@
 
                 <div class="title text-center pb-3">Quantidade de Turmas</div>
 
-                <v-card-text>
-                  <p>1</p>
+                <v-card-text class="title text-center">
+                  <p>
+                    Registrado(s)
+                    <v-spacer></v-spacer>
+                    <v-avatar color="primary" size="48">
+                      <span class="white--text headline">1</span>
+                    </v-avatar>
+                  </p>
                 </v-card-text>
               </v-card>
             </v-col>
@@ -24,8 +30,14 @@
 
                 <div class="title text-center pb-3">Quantidade de Professores</div>
 
-                <v-card-text>
-                  <p>2</p>
+                <v-card-text class="title text-center">
+                  <p>
+                    Registrado(s)
+                    <v-spacer></v-spacer>
+                    <v-avatar color="primary" size="48">
+                      <span class="white--text headline">2</span>
+                    </v-avatar>
+                  </p>
                 </v-card-text>
               </v-card>
             </v-col>
@@ -37,8 +49,14 @@
 
                 <div class="title text-center pb-3">Quantidade de Alunos</div>
 
-                <v-card-text>
-                  <p>3</p>
+                <v-card-text class="title text-center">
+                  <p>
+                    Registrado(s)
+                    <v-spacer></v-spacer>
+                    <v-avatar color="primary" size="48">
+                      <span class="white--text headline">3</span>
+                    </v-avatar>
+                  </p>
                 </v-card-text>
               </v-card>
             </v-col>
@@ -50,8 +68,14 @@
 
                 <div class="title text-center pb-3">Quantidade de Coordenadores</div>
 
-                <v-card-text>
-                  <p>4</p>
+                <v-card-text class="title text-center">
+                  <p>
+                    Registrado(s)
+                    <v-spacer></v-spacer>
+                    <v-avatar color="primary" size="48">
+                      <span class="white--text headline">4</span>
+                    </v-avatar>
+                  </p>
                 </v-card-text>
               </v-card>
             </v-col>
